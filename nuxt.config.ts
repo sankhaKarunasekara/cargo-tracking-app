386c1777ace346caa7ffd9a68697b6dc90b47ebe
Sankha Lakshan Karunasekara <sankhadlk@gmail.com>
2025-04-05 05:32:42 +0530

    Add Nuxt configuration with Tailwind and shadcn-nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
+})
